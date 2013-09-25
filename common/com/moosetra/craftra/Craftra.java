98a4cc678d5fad5b1cc4fcb2a3a110c2d9c85f59
moosetra <wpgcanuck@gmail.com>
2013-09-24 21:20:24 -0700

    Added new Block: Tarmac(TarmacBlock)

--- common/com/moosetra/craftra/Craftra.java
+++ common/com/moosetra/craftra/Craftra.java
@@ -1,10 +1,16 @@
 package com.moosetra.craftra;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.block.Block;
+import net.minecraft.item.crafting.FurnaceRecipes;
+
+import com.moosetra.craftra.block.TarmacBlock;
 import com.moosetra.craftra.lib.Reference;
 import com.moosetra.craftra.proxy.CommonProxy;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -14,6 +20,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
  
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
@@ -21,44 +28,41 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class Craftra {
 	
+	public final static Block TarmacBlock = new TarmacBlock(500, Material.rock).setHardness(1.5f).setStepSound(Block.soundStoneFootstep).setUnlocalizedName("TarmacBlock").setCreativeTab(CreativeTabs.tabBlock); 
 	
-    @Instance("craftra")
-    public static Craftra instance;
+    	@Instance("craftra")
+    	public static Craftra instance;
     
    
-    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
-	public static CommonProxy proxy;
+    	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    	public static CommonProxy proxy;
    
     
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
+    	@EventHandler
+    	public void preInit(FMLPreInitializationEvent event) {
             	
-    }
-    @EventHandler
-    public void load(FMLInitializationEvent event) {
-            proxy.registerRenderers();
+    	}
+    	@EventHandler
+    	public void load(FMLInitializationEvent event) {
+    			proxy.registerRenderers();
                         
-           ItemStack dirtStack = new ItemStack(Block.dirt);
-           ItemStack diamondStack = new ItemStack(Item.diamond, 64);
-           ItemStack blackWoolStack = new ItemStack(Block.cloth, 10, 15);
-           ItemStack stoneStack = new ItemStack(1, 32, 0);
-           ItemStack gravelStack = new ItemStack(Block.gravel);
-           
-           GameRegistry.addShapelessRecipe(diamondStack, dirtStack);
-           GameRegistry.addShapelessRecipe(stoneStack, diamondStack, dirtStack);
-           
-           GameRegistry.addRecipe(diamondStack, "xy", "yx", 
-        		   'x', dirtStack, 'y', stoneStack);
-           GameRegistry.addRecipe(gravelStack , "xyx", "y y", "xyx", 
-        		   'x', dirtStack, 'y', diamondStack);
+    			GameRegistry.registerBlock(TarmacBlock, "TarmacBlock");
+    			LanguageRegistry.addName(TarmacBlock, "Tarmac");
+    			
+          //ItemStack stackname = new ItemStack(block./item., amount, metadata), 
+          //FurnaceRecipies.smelting().addSmelting(blockID, metadata, outputitemstack, xp); 
+          //GameRegistry.addSmelting(input, output, xp);
+          //GameRegistry.addShapelessRecipe(Result, Required item, Diff Required Item);
+          //GameRegistry.addRecipe(gravelStack , Top"xyx", Mid"y y", Bottom"xyx", 
+          //		   'x', Define Variable, 'y', Define Variable);
              
-    }
+    	}
    
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
+    	@EventHandler
+    	public void postInit(FMLPostInitializationEvent event) {
       
     	
-    }
+      }
    
 
 }
---
+++ common/com/moosetra/craftra/block/TarmacBlock.java
@@ -0,0 +1,13 @@
+package com.moosetra.craftra.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+
+public class TarmacBlock extends Block {
+
+	public TarmacBlock(int id, Material material) {
+		super(id, material);
+	}
+	
+}
