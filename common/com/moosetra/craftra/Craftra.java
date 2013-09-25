fb3a3f8822f9d75a57f44fb8dc18668bd08651dc
moosetra <wpgcanuck@gmail.com>
2013-09-24 19:49:36 -0700

    Random Test Recipes

--- common/com/moosetra/craftra/Craftra.java
+++ common/com/moosetra/craftra/Craftra.java
@@ -1,5 +1,8 @@
 package com.moosetra.craftra;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.block.Block;
 import com.moosetra.craftra.lib.Reference;
 import com.moosetra.craftra.proxy.CommonProxy;
 import cpw.mods.fml.common.Mod;
@@ -10,7 +13,9 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
  
+
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
 
@@ -33,6 +38,20 @@ public class Craftra {
     public void load(FMLInitializationEvent event) {
             proxy.registerRenderers();
                         
+           ItemStack dirtStack = new ItemStack(Block.dirt);
+           ItemStack diamondStack = new ItemStack(Item.diamond, 64);
+           ItemStack blackWoolStack = new ItemStack(Block.cloth, 10, 15);
+           ItemStack stoneStack = new ItemStack(1, 32, 0);
+           ItemStack gravelStack = new ItemStack(Block.gravel);
+           
+           GameRegistry.addShapelessRecipe(diamondStack, dirtStack);
+           GameRegistry.addShapelessRecipe(stoneStack, diamondStack, dirtStack);
+           
+           GameRegistry.addRecipe(diamondStack, "xy", "yx", 
+        		   'x', dirtStack, 'y', stoneStack);
+           GameRegistry.addRecipe(gravelStack , "xyx", "y y", "xyx", 
+        		   'x', dirtStack, 'y', diamondStack);
+             
     }
    
     @EventHandler
