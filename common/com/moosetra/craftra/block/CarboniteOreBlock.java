5fd3b0db65f3dfd24c65db57161fa3ad96b162b0
moosetra <wpgcanuck@gmail.com>
2013-11-12 18:47:01 -0800

    Fixed Textures and made Carbonite ore drop xp

--- common/com/moosetra/craftra/block/CarboniteOreBlock.java
+++ common/com/moosetra/craftra/block/CarboniteOreBlock.java
@@ -1,9 +1,14 @@
 package com.moosetra.craftra.block;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
 
+import com.moosetra.craftra.Craftra;
 import com.moosetra.craftra.lib.Reference;
 
 public class CarboniteOreBlock extends Block {
@@ -17,8 +22,30 @@ public class CarboniteOreBlock extends Block {
 		setCreativeTab(CreativeTabs.tabBlock);
 		setTextureName(Reference.MOD_NAME.toLowerCase() + ":CarboniteOre");
 		setLightValue(0.5f);
+	
 		
-	}		
+	}	
+	
+	public int idDropped(int metadata, Random random, int fortune) {
+		return Craftra.CarboniteOreBlock.blockID;
+		
+	}
+	
+	public void dropBlockAsItemWithChance(World world, int par2, int par3, int par4, int par5, float par6, int par7) {
+		
+		super.dropBlockAsItemWithChance(world, par2, par3, par4, par5, par6, par7);
+		
+		if (this.idDropped(par5, world.rand, par7) != this.blockID){
+			
+			int xp = 0;
+			xp = MathHelper.getRandomIntegerInRange(world.rand, 2, 10);
+			
+			this.dropXpOnBlockBreak(world, par2, par3, par4, xp);
+			
+		}
+		
+	} 
+		
 	
 		
 }
